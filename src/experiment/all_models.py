96ce0ff9dc0e9bdaaebffd9d3ec07b71051b9ca2
soumic <fahim.farhan@outlook.com>
2025-07-12 22:31:35 +0600

    :hammer: Experimental model HyenaDNAWithDropoutBatchNorm1d, and frozen hyena layer

--- src/experiment/all_models.py
+++ src/experiment/all_models.py
@@ -159,6 +159,108 @@ class HyenaDNAWithDropoutNormAndFrozenHyena(nn.Module):
             attentions=None
         )
 
+class HyenaDNAWithDropoutBatchNorm1d(nn.Module):
+    def __init__(
+            self,
+            model_name,
+            dropout_prob: float=0.25,
+            criterion_label_smoothening: float = 0.1,
+    ):
+        super().__init__()
+        self.variant = "HyenaDNAWithDropoutBatchNorm1d"
+
+        self.base_model = AutoModelForSequenceClassification.from_pretrained(
+            model_name,
+            trust_remote_code=True,
+        )
+
+        self.hyena = self.base_model.hyena
+        self.dropout = nn.Dropout(dropout_prob)
+        self.norm = nn.BatchNorm1d(256) # nn.LayerNorm(self.base_model.hidden_size)
+        self.score = self.base_model.score
+
+        self.criterion = nn.CrossEntropyLoss(label_smoothing=criterion_label_smoothening)
+        pass
+
+    def forward(self, input_ids=None, attention_mask=None, labels=None, **kwargs):
+        outputs: BaseModelOutputWithNoAttention = self.hyena(
+            input_ids=input_ids,
+            # attention_mask=attention_mask, # hyena doesn't have attention
+            **kwargs
+        )
+        hidden: Tensor = outputs[0]  # [batch_size, hidden_dim]
+        hidden = hidden.permute(0, 2, 1)  # [B, C, L]
+        hidden = self.norm(hidden)
+        hidden = hidden.permute(0, 2, 1)  # [B, L, C] back
+        hidden = self.dropout(hidden)
+        logits = self.score(hidden)
+        pooled = logits.mean(dim=1)  # [B, C]
+
+        loss = None
+        if labels is not None:
+            loss = self.criterion(pooled, labels)
+
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=pooled,
+            hidden_states=None,
+            attentions=None
+        )
+
+class HyenaDNAWithDropoutBatchNorm1dAndFrozenHyena(nn.Module):
+    def __init__(
+            self,
+            model_name,
+            dropout_prob: float=0.25,
+            criterion_label_smoothening: float = 0.1,
+    ):
+        super().__init__()
+        self.variant = "HyenaDNAWithDropoutBatchNorm1dAndFrozenHyena"
+
+        self.base_model = AutoModelForSequenceClassification.from_pretrained(
+            model_name,
+            trust_remote_code=True,
+        )
+
+        self.hyena = self.base_model.hyena
+        self.dropout = nn.Dropout(dropout_prob)
+        self.norm = nn.BatchNorm1d(256) # nn.LayerNorm(self.base_model.hidden_size)
+        self.score = self.base_model.score
+
+        self.criterion = nn.CrossEntropyLoss(label_smoothing=criterion_label_smoothening)
+
+        # Freeze base HyenaDNA model weights
+        freeze_module(self.hyena)
+        count_parameters(self)
+        pass
+
+
+
+    def forward(self, input_ids=None, attention_mask=None, labels=None, **kwargs):
+        outputs: BaseModelOutputWithNoAttention = self.hyena(
+            input_ids=input_ids,
+            # attention_mask=attention_mask, # hyena doesn't have attention
+            **kwargs
+        )
+        hidden: Tensor = outputs[0]  # [batch_size, hidden_dim]
+        hidden = hidden.permute(0, 2, 1)  # [B, C, L]
+        hidden = self.norm(hidden)
+        hidden = hidden.permute(0, 2, 1)  # [B, L, C] back
+        hidden = self.dropout(hidden)
+        logits = self.score(hidden)
+        pooled = logits.mean(dim=1)  # [B, C]
+
+        loss = None
+        if labels is not None:
+            loss = self.criterion(pooled, labels)
+
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=pooled,
+            hidden_states=None,
+            attentions=None
+        )
+
 
 def getModel(args: Namespace) -> nn.Module:
     base_model_name = args.MODEL_NAME
@@ -185,6 +287,18 @@ def getModel(args: Namespace) -> nn.Module:
 
         if model_variant == "HyenaDNAWithDropoutNormAndFrozenHyena":
             return HyenaDNAWithDropoutNormAndFrozenHyena(
+                model_name=base_model_name,
+                dropout_prob=args.DROP_OUT_PROBABILITY,
+                criterion_label_smoothening=args.CRITERION_LABEL_SMOOTHENING,
+            )
+        if model_variant == "HyenaDNAWithDropoutBatchNorm1d":
+            return HyenaDNAWithDropoutBatchNorm1d(
+                model_name=base_model_name,
+                dropout_prob=args.DROP_OUT_PROBABILITY,
+                criterion_label_smoothening=args.CRITERION_LABEL_SMOOTHENING,
+            )
+        if model_variant == "HyenaDNAWithDropoutBatchNorm1dAndFrozenHyena":
+            return HyenaDNAWithDropoutBatchNorm1dAndFrozenHyena(
                 model_name=base_model_name,
                 dropout_prob=args.DROP_OUT_PROBABILITY,
                 criterion_label_smoothening=args.CRITERION_LABEL_SMOOTHENING,
