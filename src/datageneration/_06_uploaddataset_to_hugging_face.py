f38b660231fff55715882b784b76a02c8f2fbade
soumic <fahim.farhan@outlook.com>
2025-08-08 20:56:51 +0600

    :hammer: New dataset generation with hg19

--- src/datageneration/_06_uploaddataset_to_hugging_face.py
+++ src/datageneration/_06_uploaddataset_to_hugging_face.py
@@ -1,13 +1,14 @@
 from datasets import load_dataset, DatasetDict
 
 if __name__ == "__main__":
-    sizes = [128, 256, 512, 515, 1024, 1027, 2048, 2051, 4096, 4099]
+    sizes = [128, 256, 512, 515, 1024, 1027, 2000, 2048, 2051] # , 4096, 4099
     splits = ["train", "validate", "test"]
+    genome = "hg19" # "hg38"
 
     data_files = {}
 
     for size in sizes:
-        folder_name = f"_{size}_"
+        folder_name = f"{genome}/_{size}_"
         for split in splits:
             key = f"{split}_binned_{size}"
             filename = f"{folder_name}/_{size}_{split}_binned.csv"
@@ -17,5 +18,5 @@ if __name__ == "__main__":
     dataset = load_dataset("csv", data_files=data_files)
 
     # Push dataset to Hugging Face Hub
-    dataset.push_to_hub("fahimfarhan/mqtl-classification-datasets")
+    dataset.push_to_hub(f"fahimfarhan/mqtl-classification-datasets-{genome}")
 
